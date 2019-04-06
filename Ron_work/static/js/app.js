381e91287a6d9280ce3c12a944824e856ec7715d
thisGuyRon <42878005+thisGuyRon@users.noreply.github.com>
2019-04-06 11:47:02 -0400

    update 4-6

--- Ron_work/static/js/app.js
+++ Ron_work/static/js/app.js
@@ -16,10 +16,9 @@ function handleClick(){
 
     var inputType = d3.select("#selectType");
     var inputPrice = d3.select("#selectPrice");
-    var inputZip = d3.select("#selectZip")
     var filterType=inputType.property("value");
     var filterPrice = inputPrice.property("value");
-    var filterZip = inputZip.property("value");
+
     console.log(filterType);
     d3.json("static/js/final.json", function(data) {
         var filterRest = data;
@@ -32,11 +31,7 @@ function handleClick(){
         if (filterPrice){
             filterRest = filterRest.filter(rest => rest.price === filterPrice);
             console.log(filterRest);
-        }/*
-        if (filterZip){
-            filterRest = filterRest.filter(rest => rest.zip === filterZip);
-            console.log(filterRest);
-        }*/
+        }
         console.log(filterRest);
         filterRest.forEach(function(row) {
             var restRow = tbody.append("tr");
@@ -63,24 +58,20 @@ function init(){
 
     var selectType = d3.select("#selectType");
     var selectPrice = d3.select("#selectPrice");
-    var selectZip = d3.select("#selectZip");
 
     d3.json("static/js/final.json", function(data) {
         console.log(data);
         data.forEach(function(row) {
             rType.push(row.type);
             rPrice.push(row.price);
-            rZip.push(row.zip);
             rType.sort();
             rPrice.sort();
-            rZip.sort();
             
             }) 
             var uniqueType = new Set(rType);
             var uniquePrice = new Set(rPrice);
             var uniqueZip = new Set(rZip);
             console.log(uniquePrice);
-            console.log(uniqueZip);
             uniqueType.forEach((kind)=>{
                 selectType
                 .append("option")
@@ -94,12 +85,6 @@ function init(){
                 .property("value", dollar);
             });
 
-            uniqueZip.forEach((zips)=>{
-                selectZip
-                .append("option")
-                .text(zips)
-                .property("value", zips);
-            });
 
     });
     d3.json("static/js/final.json", function(data) {
