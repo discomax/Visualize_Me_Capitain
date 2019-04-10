b297f69ca90bf09d0a6426822ec09963a0b4ab81
nacruzz <nathan.cruz1292@gmail.com>
2019-04-10 18:16:41 -0400

    Final add

--- static/js/logic_map.js
+++ static/js/logic_map.js
@@ -1,150 +1,17 @@
-// var restaurants = [
-//     {
-//        id:"yrUDeng1LrDvgyyjI5GiHA",
-//        name:"Pho Quynh",
-//        type:"Vietnamese",
-//        rating:4.5,
-//        lat:35.211296081543,
-//        long:-80.7613372802734,
-//        price:"$$",
-//        address:"4900 Central Ave",
-//        city:"Charlotte",
-//        zip:28205
-//     },
-//     {
-//        id:"PqcOSnCsJTT9Yd2xWKIMWw",
-//        name:"La Shish Kabob",
-//        type:"Middle Eastern",
-//        rating:4.5,
-//        lat:35.2032998,
-//        long:-80.7604348,
-//        price:"$",
-//        address:"3117-A N Sharon Amity",
-//        city:"Charlotte",
-//        zip:28205
-//     },
-//     {
-//        id:"LdhprCGxa0inFo5P812EIg",
-//        name:"Loft & Cellar",
-//        type:"Cocktail Bars",
-//        rating:3.5,
-//        lat:35.2282036722545,
-//        long:-80.8464987203479,
-//        price:"$$",
-//        address:"305 W 4th St",
-//        city:"Charlotte",
-//        zip:28202
-//     },
-//     {
-//        id:"VH3WA7a-OVzFj2K_SP4BIw",
-//        name:"The King's Kitchen",
-//        type:"American (Traditional)",
-//        rating:4,
-//        lat:35.2280098136565,
-//        long:-80.8442559630758,
-//        price:"$$",
-//        address:"129 W Trade St",
-//        city:"Charlotte",
-//        zip:28202
-//     },
-//     {
-//        id:"trAd97ig2agtkZl2cimOMQ",
-//        name:"Something Classic Cafe",
-//        type:"American (New)",
-//        rating:4,
-//        lat:35.22822,
-//        long:-80.84099,
-//        price:"$",
-//        address:"220 N Tryon St",
-//        city:"Charlotte",
-//        zip:28263
-//     },
-//     {
-//        id:"QFqtVX-lyZQgVHQrG-dhlg",
-//        name:"Diamond Restaurant",
-//        type:"Southern",
-//        rating:3.5,
-//        lat:35.21909,
-//        long:-80.81317,
-//        price:"$",
-//        address:"1901 Commonwealth Ave",
-//        city:"Charlotte",
-//        zip:28205
-//     },
-//     {
-//        id:"l9tFVg7r7QXuLnCywMQl7Q",
-//        name:"The Daily Tavern",
-//        type:"American (New)",
-//        rating:4.5,
-//        lat:35.2294337656203,
-//        long:-80.8419475618761,
-//        price:"$$",
-//        address:"214 N Church St",
-//        city:"Charlotte",
-//        zip:28202
-//     },
-//     {
-//        id:"Cbr_THdFrhePgJoqQ9X6gQ",
-//        name:"Pho Hoa Noodle Soup",
-//        type:"Vietnamese",
-//        rating:4.5,
-//        lat:35.217429,
-//        long:-80.794365,
-//        price:"$",
-//        address:"3000 Central Ave",
-//        city:"Charlotte",
-//        zip:28205
-//     },
-//     {
-//        id:"SHFlELFcEcAOJv_fTAKChQ",
-//        name:"Dandelion Market",
-//        type:"Bars",
-//        rating:4,
-//        lat:35.2286689,
-//        long:-80.84241,
-//        price:"$$",
-//        address:"118 W 5th St",
-//        city:"Charlotte",
-//        zip:28202
-//     },
-//     {
-//        id:"K3iw26MkmSBPAM7BzHkIYQ",
-//        name:"Yama Asian Fusion",
-//        type:"Japanese",
-//        rating:4,
-//        lat:35.157295,
-//        long:-80.824004,
-//        price:"$$",
-//        address:"720 Governor Morrison St",
-//        city:"Charlotte",
-//        zip:28211
-//     },
-//     {
-//        id:"bgvm73MMjC2f5qo_RgXOXg",
-//        name:"Halcyon",
-//        type:"Cocktail Bars",
-//        rating:4,
-//        lat:35.224421,
-//        long:-80.848247,
-//        price:"$$$",
-//        address:"500 S Tryon St",
-//        city:"Charlotte",
-//        zip:28202
-//     }
-// ];
+
 
 const MAP_KEY = "pk.eyJ1IjoiZGlzY29tYXgiLCJhIjoiY2p0bGtwdjdvMGN0ZzRhcGVoaHBrdGRvNiJ9.1FRcT1k-Vdo3ZLHVSp28Fg";
 // Creating map object
 
-// An array which will be used to store created restMarkers
-var restMarkers = [];
+// Arrays to store created restMarkers
+var allRestaurants = [];
+var greenRestaurants = [];
+var violetRestaurants = []; 
+var yellowRestaurants = [];
+var redRestaurants = [];
 
 // loop through the cities array, create a new marker for each city. 
 // for (var i = 0; i < restaurants.length; i++) {
-   
-// var url = "/data";
-// console.log('url: ', url)
-// d3.json(url).then(function(data) {
 d3.json("static/js/final.json", function(data) {
    
    //var restString = JSON.stringify(data);
@@ -196,44 +63,100 @@ d3.json("static/js/final.json", function(data) {
          case "$": return redMarker;
       }
    }
-   for (var i=0; i < restaurants.length; i++) {
-   // Make a variable to hold the latitude and longitude of each restaurant
-      var loc = [restaurants[i].lat, restaurants[i].long];
 
+   // Frunction popupString creates string for a restaurants pop-up.
+   function popupString(restaurant) {
+      var string = "<h2>" + restaurant.name + "</h2><hr> \
+         <p><font size='3'> \
+         Category: " + restaurant.type + "<br> \
+         Prince: " + restaurant.price + "<br> \
+         Rating: " + restaurant.rating + "<br><br> \
+         Address: <br> \
+         " + restaurant.address + "<br>" + restaurant.city + " " + restaurant.zip + "\
+         </font></p>";
+
+      return string;
+   }
+   
+   // Function chooseArray uses the price to determine what color array
+   // To push the marker to the correspondingn array.
+   function chooseArray(price, marker) {
+      switch(price) {
+         case "$$$$": greenRestaurants.push(marker);
+         case "$$$": violetRestaurants.push(marker);
+         case "$$": yellowRestaurants.push(marker);
+         case "$": redRestaurants.push(marker);
+      }
+   }
+   
+   for (var i=0; i < restaurants.length; i++) {
+   // Make a variables to hold info about each restaurant for use in markers.
+      var rName = restaurants[i].name;
+      var rLocation = [restaurants[i].lat, restaurants[i].long];
+      var rPrice = restaurants[i].price;
       // Create the restaurant's marker using the coordinates using loc,
-      // bind a pop-up with restaurant info & push it to the cityMarkers array.
-      restMarkers.push(
-         L.marker(loc, {icon: chooseMarker(restaurants[i].price), title: restaurants[i].name, riseOnHover: true})
-            .bindPopup("<h2>" + restaurants[i].name + "</h2><hr> \
-               <p><font size='3'> \
-               Category: " + restaurants[i].type + "<br> \
-               Prince: " + restaurants[i].price + "<br> \
-               Rating: " + restaurants[i].rating + "<br><br> \
-               Address: <br> \
-               " + restaurants[i].address + "<br>" + restaurants[i].city + " " + restaurants[i].zip + "\
-               </font></p>")
-      );
+      // Bind a pop-up with restaurant info & push it to the allRestaurants array.
+      var restaurantMarker = L.marker(rLocation, {icon: chooseMarker(rPrice), title: rName, riseOnHover: true})
+         .bindPopup(popupString(restaurants[i]));
+      // Push marker to the allRestaurants array.
+      allRestaurants.push(restaurantMarker);
+      // Use chooseArray to push marker to corresponding color array.
+      chooseArray(rPrice, restaurantMarker);
+      
    }
 
-   console.log(restMarkers);
-   // Add all the restaurant markers to the a new layerGroup.
-   var restLayer = L.layerGroup(restMarkers);
-
-   // Adding tile layer
+   // Create variables for the base layers.
    var streetMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
       attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
       maxZoom: 18,
       id: "mapbox.streets",
       accessToken: MAP_KEY
    });
-   var mapLat, mapLong;
-   mapLat = 35.227085;
-   mapLong = -80.843124;
+
+   var darkMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+      maxZoom: 18,
+      id: "mapbox.dark",
+      accessToken: MAP_KEY
+});
+   // Use the marker arrays to create layers.
+   var allLayer = L.layerGroup(allRestaurants);
+   var greenLayer = L.layerGroup(greenRestaurants);
+   var violetLayer = L.layerGroup(violetRestaurants);
+   var yellowLayer = L.layerGroup(yellowRestaurants);
+   var redLayer = L.layerGroup(redRestaurants);
+   
+   // Create a baseMaps object
+   var baseMaps = {
+      "Street Map": streetMap,
+      "Dark Map": darkMap
+   };
+
+   // Create an overlay object
+   var overlayMaps = {
+      "All Restaurants": allLayer,
+      "Price: $": redLayer,
+      "Price: $$": yellowLayer,
+      "Price: $$$": violetLayer,
+      "Price: $$$$": greenLayer
+   };
+
+   //Create the map with Charlotte's lat and long.
+   var charlotteLat, charlotteLong;
+   charlotteLat = 35.227085;
+   charlotteLong = -80.843124;
 
    var restaurantMap = L.map("map", {
-      center: [mapLat, mapLong],
-      zoom: 11,
-      layers: [streetMap, restLayer]
+      center: [charlotteLat, charlotteLong],
+      zoom: 10.25,
+      layers: [streetMap, allLayer]
    });
-});
+
+   // Create the layer control and add to map
+   L.control.layers(baseMaps, overlayMaps, {
+         collapsed: false
+      }).addTo(restaurantMap);
+   });
+
+
 
