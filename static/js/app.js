5fa9ad8c26a4b7725b0a9aa2a52413c504e4bb21
Mike Patterson <mpatterson73@gmail.com>
2019-11-06 12:37:39 -0500

    fixed mergre conflicts

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,10 @@
-
+// initial init function call to load the drop down values
 init();
+//initial d3 object captues
 var tbody = d3.select("tbody");
 var button = d3.select("#filter-btn");
 
+//even listener for filter button click
 button.on("click", handleClick);
 
 // function to handle button click
@@ -11,29 +13,31 @@ function handleClick(){
     d3.event.preventDefault();
     // clear existing records
     tbody.html("");
-
+    //d3 objects and variables to grab data from event click
     var inputType = d3.select("#selectType");
     var inputPrice = d3.select("#selectPrice");
     var filterType=inputType.property("value");
     var filterPrice = inputPrice.property("value");
 
-    // var url = "/data";
-    // console.log('url: ', url)
+
     console.log(filterType);
-    // d3.json(url).then(function(data) {
     d3.json("static/js/final.json", function(data) {
+        //set variable to begin filter data
         var filterRest = data;
         console.log(filterRest);
+
+        //if user did not select restaurant type, ignore filtering
        if(filterType){
             filterRest = filterRest.filter(rest => rest.type === filterType);
             console.log(filterRest);
         }
-
+        //if user did not select price, ignore filtering
         if (filterPrice){
             filterRest = filterRest.filter(rest => rest.price === filterPrice);
             console.log(filterRest);
         }
         console.log(filterRest);
+        //loop and bind data to table
         filterRest.forEach(function(row) {
             var restRow = tbody.append("tr");
             var restName = restRow.append("td").text(row.name);
@@ -44,23 +48,20 @@ function handleClick(){
             var restCity    =restRow.append("td").text(row.city);
             var restZip = restRow.append("td").text(row.zip);
             
-            })     
-        
-        
-        
-            }) 
-
+        })     
+    }) 
 }
 
+//function to list filtering population
 function init(){
     var rType = [];
     var rPrice = [];
-    var rZip = [];
 
     var selectType = d3.select("#selectType");
     var selectPrice = d3.select("#selectPrice");
 
-    // d3.json(url).then(function(data) {
+    //add restaurant types to list
+    //add restaurant price to list
     d3.json("static/js/final.json", function(data) {
         console.log(data);
         data.forEach(function(row) {
@@ -70,10 +71,12 @@ function init(){
             rPrice.sort();
             
             }) 
+            //remove duplicates
             var uniqueType = new Set(rType);
             var uniquePrice = new Set(rPrice);
             var uniqueZip = new Set(rZip);
             console.log(uniquePrice);
+            //push to dropdown
             uniqueType.forEach((kind)=>{
                 selectType
                 .append("option")
@@ -89,7 +92,7 @@ function init(){
 
 
     });
-    // d3.json(url).then(function(data) {
+    // initial load of list for all restaurants
     d3.json("static/js/final.json", function(data) {
         data.forEach(function(row) {
             var restRow = tbody.append("tr");
